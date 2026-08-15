c020a050915ed3cc98ed184cebc04e599e17dfab
Arish limbu <limbuarish0@gmail.com>
2026-08-15 21:43:17 +0545

    new

---
+++ first.py
@@ -0,0 +1,14 @@
+print("Hello World")
+print("I am",25)
+print("My name is: ",end="")
+print("Arish Limbu")
+x=2
+y=3
+z=float(12)
+if 5 > 1:
+    print("HI")
+if 5==5:
+    print (x,z)
+x='hello'
+print(x)
+
