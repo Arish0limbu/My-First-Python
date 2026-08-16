713011bed0b395617dbd8192e7bcf5865ad0474b
Arish limbu <limbuarish0@gmail.com>
2026-08-16 21:32:28 +0545

    new

--- first.py
+++ first.py
@@ -1,24 +1,32 @@
 import os
 
+
 def cls():
-        os.system("cls")
+    os.system("cls")
+
 
 def line():
-        for i in range(100):
-            print("=",end="")
-        print(" ")
-        
+    for i in range(100):
+        print("=", end="")
+    print()
+
+
 cls()
 line()
-print("Enter Your name: ",end="")
-y=input()
-print("Enter your age: ",end="")
-x=input()
+
+print("Enter Your name: ", end="")
+y = input()
+y = y.title()
+
+print("Enter your age: ", end="")
+x = input()
+
 line()
 cls()
 line()
-print(y.capitalize(),"is your name.")
-z=f"Your age is {x}"
-print(z)
+
+print(y, "is your name.")
+print(f"Your age is {x}")
+
 line()
 os.system("pause")
