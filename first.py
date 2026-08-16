24e17b5fb95d87389bbf2ed1b1eabecfa93dd681
Arish limbu <limbuarish0@gmail.com>
2026-08-16 20:30:07 +0545

    new

--- first.py
+++ first.py
@@ -1,14 +1,4 @@
-print("Hello World")
-print("I am",25)
-print("My name is: ",end="")
-print("Arish Limbu")
-x=2
-y=3
-z=float(12)
-if 5 > 1:
-    print("HI")
-if 5==5:
-    print (x,z)
-x='hello'
-print(x)
-
+import os
+x=22
+z=f"I am {x} years old"
+print(z.upper())
