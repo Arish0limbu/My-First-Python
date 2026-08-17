c3ba5fedaaf31ea1469ea1f420c427f4db9bb913
Arish limbu <limbuarish0@gmail.com>
2026-08-17 19:42:00 +0545

    start

---
+++ bool.py
@@ -0,0 +1,41 @@
+import os
+
+def line(l=100):
+    for i in range(l):
+        print("=",end="")
+    print(" ")
+
+def center(c=44):
+    for i in range(c):
+        print(" ",end="")
+    
+def cls():
+    os.system("cls")
+    
+def pause():
+    os.system("pause")
+
+cls()
+line()
+center(38)
+print("|| Enter your details ||")
+line()
+
+print("Enter your name: ",end="")
+name=input()
+print("Enter your age: ",end="")
+age=input()
+line()
+pause()
+
+cls()
+line()
+center(42)
+print("|| Your details ||")
+line()
+print("Name: "+name.title())
+print("Age: ",age)
+line()
+pause()
+
+
