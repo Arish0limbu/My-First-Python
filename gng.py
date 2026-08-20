8790a5b1bce69b59c2f12d903138d5904d41d297
Arish limbu <limbuarish0@gmail.com>
2026-08-20 21:31:26 +0545

    in

---
+++ gng.py
@@ -0,0 +1,40 @@
+import os
+
+def line(c=100):
+    for i in range(c):
+        print("=",end="")
+    print(" ")
+
+def center(c=45):
+    for i in range(c):
+        print(" ",end="")
+
+def cls():
+    os.system("clear")
+
+def pause():
+    os.system("pause")
+        
+def head(c=45):
+    cls()
+    line()
+    center(c)
+
+head()
+print("Enter your details")
+line()
+
+print("Enter your name: ",end="")
+name = input()
+line()
+pause()
+
+head(43)
+print("|| Checking ||")
+line()
+
+if 'a' not in name or 'A' not in name:
+    print(name.capitalize(),"Verified.....")
+else:
+    print("No one verified..........")
+line()   
